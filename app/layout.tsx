53460bd311d863d666a6ed78f889b3915cceb80e
Prof.zi <manlikefemzi@gmail.com>
2023-07-10 07:27:36 +0100

    status fix 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,24 +4,24 @@ import ActiveStatus from './components/ActiveStatus'
 import ToasterContext from './context/ToasterContext'
 
 export const metadata = {
-  title: 'Balablu',
-  description: 'Balablu',
+    title: 'Balablu',
+    description: 'Balablu',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body>
-        <AuthContext>
-          <ToasterContext />
-          {/* <ActiveStatus /> */}
-          {children}
-        </AuthContext>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body>
+                <AuthContext>
+                    <ToasterContext />
+                    <ActiveStatus />
+                    {children}
+                </AuthContext>
+            </body>
+        </html>
+    )
 }
